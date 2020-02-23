4e10a3981d4fdecbf1a77cb330190b114a4b5c4a
Adi52 <32686981+Adi52@users.noreply.github.com>
2020-02-23 14:06:54 +0100

    Add bot

--- connect_four_19/connect_four.py
+++ connect_four_19/connect_four.py
@@ -1,6 +1,6 @@
 import time
+import random
 from board_class import Board
-
 
 board = Board()
 
@@ -10,31 +10,37 @@ def incorrect_input():
     time.sleep(2)
 
 
+def bot_choice():
+    return random.randint(1, 7)
+
+
 game_over = False
+turn = 0
 
 print('Zaczynaj! wybierz kolumne 1-7 w którą chcesz wrzucić żeton.')
 print('X - Ty')
 print('Y - przeciwnik')
 while not game_over:
+    turn = turn % 2
     board.display_board()
-
-    try:
-        user_choice = int(input("Wybieram: "))
-        if user_choice > 7 or user_choice < 1:
+    if turn == 0:
+        try:
+            user_choice = int(input("Wybieram: "))
+            if user_choice > 7 or user_choice < 1:
+                incorrect_input()
+                continue
+        except ValueError:
             incorrect_input()
             continue
-    except ValueError:
-        incorrect_input()
-        continue
+    if turn == 1:
+        user_choice = bot_choice()
 
     if board.is_valid_location(user_choice):
-        board.drop_piece(user_choice, 1)
+        board.drop_piece(user_choice, turn+1)
     else:
         print('Już jest pełno!')
 
+    time.sleep(2)
+    turn += 1
 
-
-
-#Potem dodaj warunek wygranej: 4 na skos, 4 pion, 4 poziom
-
-
+# Potem dodaj warunek wygranej: 4 na skos, 4 pion, 4 poziom
---
+++ connect_four_19/tests.py
@@ -0,0 +1,11 @@
+import random
+
+turn = 1
+
+for i in range(15):
+    turn = turn%2
+    print(turn)
+    turn += 1
+
+
+x = random()
