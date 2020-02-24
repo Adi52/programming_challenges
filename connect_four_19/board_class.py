2ea2a3fdea13696aff017d14df6a5bfbd1e5902e
Adi52 <32686981+Adi52@users.noreply.github.com>
2020-02-24 10:37:49 +0100

    Add win conditions.

--- connect_four_19/board_class.py
+++ connect_four_19/board_class.py
@@ -13,9 +13,9 @@ class Board():
         self.COLUMN_COUNT = 7
 
     def display_board(self):
-        print(" 1  2  3  4  5  6  7")
+        print("1 2 3 4 5 6 7")
         for i in self.board:
-            print(i)
+            print(*i, sep='|')
 
     def get_next_open_row(self, user_choice):
         result = -1
@@ -31,3 +31,30 @@ class Board():
     def is_valid_location(self, user_choice):
         return self.board[0][user_choice - 1] == 0
 
+    def winning_condition(self, turn):
+        #horizontal
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(self.ROW_COUNT):
+                if (self.board[row][column] == turn + 1 and self.board[row][column+1] == turn + 1 and
+                        self.board[row][column+2] == turn + 1 and self.board[row][column+3] == turn + 1):
+                    return True
+        #vertical
+        for column in range(self.COLUMN_COUNT):
+            for row in range(self.ROW_COUNT-3):
+                if (self.board[row][column] == turn + 1 and self.board[row+1][column] == turn + 1 and
+                        self.board[row+2][column] == turn + 1 and self.board[row+3][column] == turn + 1):
+                    return True
+
+        #positively slopped diagonals
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(self.ROW_COUNT-3):
+                if (self.board[row][column] == turn + 1 and self.board[row+1][column+1] == turn + 1 and
+                        self.board[row+2][column+2] == turn + 1 and self.board[row+3][column+3] == turn + 1):
+                    return True
+
+        #negatively slopped diagonals
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(self.ROW_COUNT-3):
+                if (self.board[row][column] == turn + 1 and self.board[row+1][column+1] == turn + 1 and
+                        self.board[row+2][column+2] == turn + 1 and self.board[row+3][column+3] == turn + 1):
+                    return True
--- connect_four_19/connect_four.py
+++ connect_four_19/connect_four.py
@@ -4,6 +4,7 @@ from board_class import Board
 
 board = Board()
 
+cls = lambda: print('\n' * 1000)
 
 def incorrect_input():
     print('Możesz wpisać tylko liczbę 1-7!')
@@ -18,11 +19,12 @@ game_over = False
 turn = 0
 
 print('Zaczynaj! wybierz kolumne 1-7 w którą chcesz wrzucić żeton.')
-print('X - Ty')
-print('Y - przeciwnik')
+print('1 - Ty')
+print('2 - przeciwnik')
+board.display_board()
 while not game_over:
     turn = turn % 2
-    board.display_board()
+
     if turn == 0:
         try:
             user_choice = int(input("Wybieram: "))
@@ -35,12 +37,25 @@ while not game_over:
     if turn == 1:
         user_choice = bot_choice()
 
+
     if board.is_valid_location(user_choice):
         board.drop_piece(user_choice, turn+1)
+        board.display_board()
+
+        if board.winning_condition(turn):
+            print('Wygrana gracza {}!'.format(turn + 1))
+            game_over = True
+
+        turn += 1
+        time.sleep(1)
+
     else:
         print('Już jest pełno!')
+        time.sleep(3)
 
-    time.sleep(2)
-    turn += 1
+
+
+    #cls()
+
 
 # Potem dodaj warunek wygranej: 4 na skos, 4 pion, 4 poziom
