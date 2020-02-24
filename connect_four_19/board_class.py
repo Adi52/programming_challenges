b8a4c03182af0ec18a612c9fcd9ec89e436ae1a6
Adi52 <32686981+Adi52@users.noreply.github.com>
2020-02-24 17:48:33 +0100

    Game functions

--- connect_four_19/board_class.py
+++ connect_four_19/board_class.py
@@ -10,8 +10,52 @@ class Board():
 
         self.ROW_COUNT = 6
         self.COLUMN_COUNT = 7
+        self.board_color = (0, 123, 255)
+
 
     def display_board(self):
         print("1 2 3 4 5 6 7")
         for i in self.board:
             print(*i, sep='|')
+
+    def get_next_open_row(self, user_choice):
+        result = -1
+        for i in range(self.ROW_COUNT):
+            if self.board[i][user_choice - 1] == 0:
+                result += 1
+        return result
+
+    def drop_piece(self, user_choice, piece):
+        row = self.get_next_open_row(user_choice)
+        self.board[row][user_choice - 1] = piece
+
+    def is_valid_location(self, user_choice):
+        return self.board[0][user_choice - 1] == 0
+
+    def winning_condition(self, turn):
+        #horizontal
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(self.ROW_COUNT):
+                if (self.board[row][column] == turn + 1 and self.board[row][column+1] == turn + 1 and
+                        self.board[row][column+2] == turn + 1 and self.board[row][column+3] == turn + 1):
+                    return True
+        #vertical
+        for column in range(self.COLUMN_COUNT):
+            for row in range(self.ROW_COUNT-3):
+                if (self.board[row][column] == turn + 1 and self.board[row+1][column] == turn + 1 and
+                        self.board[row+2][column] == turn + 1 and self.board[row+3][column] == turn + 1):
+                    return True
+
+        #positively slopped diagonals
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(self.ROW_COUNT-3):
+                if (self.board[row][column] == turn + 1 and self.board[row+1][column+1] == turn + 1 and
+                        self.board[row+2][column+2] == turn + 1 and self.board[row+3][column+3] == turn + 1):
+                    return True
+
+        #negatively slopped diagonals
+        for column in range(self.COLUMN_COUNT-3):
+            for row in range(3, self.ROW_COUNT):
+                if (self.board[row][column] == turn + 1 and self.board[row-1][column+1] == turn + 1 and
+                        self.board[row-2][column+2] == turn + 1 and self.board[row-3][column+3] == turn + 1):
+                    return True
--- connect_four_19/connect_four.py
+++ connect_four_19/connect_four.py
@@ -1,13 +1,40 @@
 import time
 import random
 import pygame
+import sys
 from settings import Settings
 from board_class import Board
-import game_functions
 
 board = Board()
 
 cls = lambda: print('\n' * 1000)
+
+
+def incorrect_input():
+    print('Możesz wpisać tylko liczbę 1-7!')
+    time.sleep(2)
+
+
+def bot_choice():
+    return random.randint(1, 7)
+
+
+def check_events(ai_settings, screen):
+    """Reakcja na zdarzenia generowane przez klawiaturę i mysz."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings, screen):
+    """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu."""
+    # odswiezenie ekranu w trakcie kazdej iteracji
+    screen.fill(ai_settings.bg_color)
+    pygame.draw.rect(screen, (0, 123, 255), pygame.Rect(0, 114, 800, 600))
+
+    # wyświetlanie ostatnio zmodyfikowanego ekranu.
+    pygame.display.flip()
+
 
 def run_game():
     pygame.init()
@@ -16,18 +43,49 @@ def run_game():
     pygame.display.set_caption("Connect four")
 
     while True:
+        check_events(ai_settings, screen)
+
+        """
+        game_over = False
+        turn = 0
+
         print('Zaczynaj! wybierz kolumne 1-7 w którą chcesz wrzucić żeton.')
         print('1 - Ty')
         print('2 - przeciwnik')
         board.display_board()
-        game_functions.start_game()
+        while not game_over:
+            turn = turn % 2
 
+            if turn == 0:
+                try:
+                    user_choice = int(input("Wybieram: "))
+                    if user_choice > 7 or user_choice < 1:
+                        incorrect_input()
+                        continue
+                except ValueError:
+                    incorrect_input()
+                    continue
+            if turn == 1:
+                user_choice = bot_choice()
 
+            if board.is_valid_location(user_choice):
+                board.drop_piece(user_choice, turn + 1)
+                board.display_board()
 
+                if board.winning_condition(turn):
+                    print('Wygrana gracza {}!'.format(turn + 1))
+                    game_over = True
 
-            #cls()
+                turn += 1
+                time.sleep(1)
+
+            else:
+                print('Już jest pełno!')
+                time.sleep(3)
+
+            # cls()
+            """
+        update_screen(ai_settings, screen)
+
 
 run_game()
-
-
-# Potem dodaj warunek wygranej: 4 na skos, 4 pion, 4 poziom
