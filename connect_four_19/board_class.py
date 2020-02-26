5927b51d0fc3cc38f423196428f5beab104bbebc
Adi52 <32686981+Adi52@users.noreply.github.com>
2020-02-26 23:10:23 +0100

    Add drop piece (py game)

--- connect_four_19/board_class.py
+++ connect_four_19/board_class.py
@@ -21,17 +21,18 @@ class Board():
     def get_next_open_row(self, user_choice):
         result = -1
         for i in range(self.ROW_COUNT):
-            if self.board[i][user_choice - 1] == 0:
+            if self.board[i][user_choice] == 0:
                 result += 1
         return result
 
     def drop_piece(self, user_choice, piece):
         row = self.get_next_open_row(user_choice)
-        self.board[row][user_choice - 1] = piece
+        self.board[row][user_choice] = piece
+
 
 
     def is_valid_location(self, user_choice):
-        return self.board[0][user_choice - 1] == 0
+        return self.board[0][user_choice] == 0
 
     def winning_condition(self, turn):
         #horizontal
--- connect_four_19/connect_four.py
+++ connect_four_19/connect_four.py
@@ -3,6 +3,7 @@ import random
 import pygame
 import pygame.gfxdraw
 import sys
+from math import floor
 from settings import Settings
 from board_class import Board
 from filledrounded_rect import AAfilledRoundedRect
@@ -21,17 +22,17 @@ def incorrect_input():
 
 
 def bot_choice():
-    return random.randint(1, 7)
+    return random.randint(0, 6)
 
-
+"""
 def check_events(ai_settings, screen):
-    """Reakcja na zdarzenia generowane przez klawiaturę i mysz."""
+    #Reakcja na zdarzenia generowane przez klawiaturę i mysz.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
-
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            return event.pos
+"""
 
 def update_screen(ai_settings, screen):
     """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu."""
@@ -44,7 +45,12 @@ def update_screen(ai_settings, screen):
 
     for column in range(board.COLUMN_COUNT):
         for row in range(board.ROW_COUNT):
-            piece.draw(column, row, ai_settings.bg_color)
+            if board.board[row][column] == 0:
+                piece.draw(column, row, ai_settings.bg_color)
+            elif board.board[row][column] == 1:
+                piece.draw(column, row, (139, 124, 0))
+            else:
+                piece.draw(column, row, (139,0,0))
 
     # wyświetlanie ostatnio zmodyfikowanego ekranu.
     pygame.display.flip()
@@ -55,52 +61,59 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Connect four")
+    game_over = False
+    turn = 0
+    """
+    print('Zaczynaj! wybierz kolumne 1-7 w którą chcesz wrzucić żeton.')
+    print('1 - Ty')
+    print('2 - przeciwnik')
+    """
+    board.display_board()
 
-    while True:
-        check_events(ai_settings, screen)
+    while not game_over:
+        update_screen(ai_settings, screen)
+        #check_events(ai_settings, screen)
+        turn = turn % 2
 
-        game_over = False
-        turn = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if turn == 0:
+                    user_choice = event.pos[0]
+                    user_choice = int(floor(user_choice/100))
+                    if user_choice > 6:
+                        user_choice = 6
+                    print(user_choice)
 
-        print('Zaczynaj! wybierz kolumne 1-7 w którą chcesz wrzucić żeton.')
-        print('1 - Ty')
-        print('2 - przeciwnik')
-        board.display_board()
-
-        while not game_over:
-            turn = turn % 2
-
-            if turn == 0:
-
-                try:
-                    user_choice = int(input("Wybieram: "))
-                    if user_choice > 7 or user_choice < 1:
+                    """
+                    try:
+                        user_choice = int(input("Wybieram: "))
+                        if user_choice > 7 or user_choice < 1:
+                            incorrect_input()
+                            continue
+                    except ValueError:
                         incorrect_input()
                         continue
-                except ValueError:
-                    incorrect_input()
-                    continue
-            if turn == 1:
-                user_choice = bot_choice()
+                    """
+                if turn == 1:
+                    user_choice = bot_choice()
 
-            if board.is_valid_location(user_choice):
-                board.drop_piece(user_choice, turn + 1)
-                board.display_board()
+                if board.is_valid_location(user_choice):
+                    board.drop_piece(user_choice, turn + 1)
+                    board.display_board()
 
-                if board.winning_condition(turn):
-                    print('Wygrana gracza {}!'.format(turn + 1))
-                    game_over = True
+                    if board.winning_condition(turn):
+                        print('Wygrana gracza {}!'.format(turn + 1))
+                        game_over = True
 
-                turn += 1
-                time.sleep(1)
-                update_screen(ai_settings, screen)
-            else:
-                print('Już jest pełno!')
-                time.sleep(3)
+                    turn += 1
+                    time.sleep(1)
+                    update_screen(ai_settings, screen)
+                else:
+                    print('Już jest pełno!')
 
             # cls()
-
-
 
 
 run_game()
